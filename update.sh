5062e8fac9adfedd980366d4fdc8b6beb50a9536
Ali Karbassi <ali@karbassi.com>
2015-06-27 16:15:36 -0500

    update.

--- update.sh
+++ update.sh
@@ -4,7 +4,7 @@ if [[ ! -a /etc/apt/sources.list.d/google-chrome.list ]]; then
   sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
 fi
 
-sudo add-apt-repository ppa:mpstark/elementary-tweaks-daily -y
+sudo add-apt-repository ppa:mpstark/elementary-tweaks-daily -y  > /dev/null
 
 echo " - Running update"
 sudo apt-get update -qq

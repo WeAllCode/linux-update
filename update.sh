a95d7e6716084d0e56d64d299025337dc1445cee
Ali Karbassi <ali@karbassi.com>
2016-07-18 14:50:35 -0500

    Script needs to run as root
    Flipped sudo commands to regular commands and user commands are run as
    the `coderdojochi` user.

--- update.sh
+++ update.sh
@@ -1,42 +1,42 @@
-notify-send --urgency=critical "Updating System"
+sudo -H -u coderdojochi bash -c 'notify-send --urgency=critical "Updating System"'
 
 # Adding Google to package manager
-notify-send --urgency=low "Adding Google to package manager"
-wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-sudo wget -qLO /etc/apt/sources.list.d/google-chrome.list "https://raw.githubusercontent.com/CoderDojoChi/linux-update/master/google-chrome.list"
+sudo -H -u coderdojochi bash -c 'notify-send --urgency=low "Adding Google to package manager"'
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | apt-key add -
+wget -qLO /etc/apt/sources.list.d/google-chrome.list "https://raw.githubusercontent.com/CoderDojoChi/linux-update/master/google-chrome.list"
 
 # Adding Elementary Tweaks to package manager
-notify-send --urgency=low "Adding Elementary Tweaks to package manager"
-sudo add-apt-repository ppa:mpstark/elementary-tweaks-daily -y
+sudo -H -u coderdojochi bash -c 'notify-send --urgency=low "Adding Elementary Tweaks to package manager"'
+add-apt-repository ppa:mpstark/elementary-tweaks-daily -y
 
 # Upgrading system
-notify-send --urgency=low "Upgrading system"
-sudo apt-get -q=2 update
-sudo apt-get -q=2 dist-upgrade -y
+sudo -H -u coderdojochi bash -c 'notify-send --urgency=low "Upgrading system"'
+apt-get -q=2 update
+apt-get -q=2 dist-upgrade -y
 
 # Uninstalling unused packages
-notify-send --urgency=low "Uninstalling unused packages"
+sudo -H -u coderdojochi bash -c 'notify-send --urgency=low "Uninstalling unused packages"'
 command -v zeitgeist-daemon &> /dev/null
 if [ $? -eq 0 ]; then
     zeitgeist-daemon --quit
 fi
-sudo apt-get -q=2 --purge autoremove -y deja-dup indicator-messages empathy-* gnome-online-accounts activity-log-manager-common activity-log-manager-control-center zeitgeist zeitgeist-core zeitgeist-datahub midori-granite noise software-center update-manager scratch-text-editor modemmanager geary
+apt-get -q=2 --purge autoremove -y deja-dup indicator-messages empathy-* gnome-online-accounts activity-log-manager-common activity-log-manager-control-center zeitgeist zeitgeist-core zeitgeist-datahub midori-granite noise software-center update-manager scratch-text-editor modemmanager geary
 
 # Installing elementary-tweaks
-notify-send --urgency=low "Installing elementary-tweaks"
-sudo apt-get -q=2 install -y elementary-tweaks
+sudo -H -u coderdojochi bash -c 'notify-send --urgency=low "Installing elementary-tweaks"'
+apt-get -q=2 install -y elementary-tweaks
 
 # Installing gedit
-notify-send --urgency=low "Installing gedit"
-sudo apt-get -q=2 install -y gedit
+sudo -H -u coderdojochi bash -c 'notify-send --urgency=low "Installing gedit"'
+apt-get -q=2 install -y gedit
 
 # Installing vim
-notify-send --urgency=low "Installing vim"
-sudo apt-get -q=2 install -y vim
+sudo -H -u coderdojochi bash -c 'notify-send --urgency=low "Installing vim"'
+apt-get -q=2 install -y vim
 
 # Installing google-chrome-stable
-notify-send --urgency=low "Installing google-chrome-beta"
-sudo apt-get -q=2 install -y google-chrome-beta
+sudo -H -u coderdojochi bash -c 'notify-send --urgency=low "Installing google-chrome-beta"'
+apt-get -q=2 install -y google-chrome-beta
 
 # sudo sed -i '/Group\]\|Desktop Entry/a StartupWMClass=Google-chrome-stable' /usr/share/applications/google-chrome.desktop
 
@@ -44,70 +44,70 @@ sudo apt-get -q=2 install -y google-chrome-beta
 # sudo sed -i 's/\[NewWindow Shortcut Group\]/[NewWindow Shortcut Group]\nStartupWMClass=Google-chrome-stable/g' /usr/share/applications/google-chrome.desktop
 # sudo sed -i 's/\[NewIncognito Shortcut Group\]/[NewIncognito Shortcut Group]\nStartupWMClass=Google-chrome-stable/g' /usr/share/applications/google-chrome.desktop
 
-sudo rm -rf ~/.config/midori ~/.config/google-chrome-beta
+rm -rf /home/coderdojochi/.config/midori /home/coderdojochi/.config/google-chrome-beta
 
-sudo wget -qLO /opt/google/chrome-beta/default_apps/external_extensions.json "https://raw.githubusercontent.com/CoderDojoChi/linux-update/master/external_extensions.json"
+wget -qLO /opt/google/chrome-beta/default_apps/external_extensions.json "https://raw.githubusercontent.com/CoderDojoChi/linux-update/master/external_extensions.json"
 
-google-chrome-beta --no-first-run > /dev/null 2>&1 &
-sleep 10
-killall chrome
-sleep 5
+sudo -H -u coderdojochi bash -c 'google-chrome-beta --no-first-run > /dev/null 2>&1 &'
+sudo -H -u coderdojochi bash -c 'sleep 10'
+sudo -H -u coderdojochi bash -c 'killall chrome'
+sudo -H -u coderdojochi bash -c 'sleep 5'
 
 # Chrome: disable password manager
-notify-send --urgency=low "Chrome: disable password manager"
-sudo sed -i 's/user",/user","password_manage_enabled":false,/' ~/.config/google-chrome-beta/Default/Preferences
+sudo -H -u coderdojochi bash -c 'notify-send --urgency=low "Chrome: disable password manager"'
+sed -i 's/user",/user","password_manage_enabled":false,/' /home/coderdojochi/.config/google-chrome-beta/Default/Preferences
 
 # Chrome: change startup URL to google.com
-notify-send --urgency=low "Chrome: change startup URL to google.com"
-sudo sed -i 's/"restore_on_startup_migrated":true,/"restore_on_startup":4,"restore_on_startup_migrated":true,"startup_urls":["https:\/\/google.com\/"],/' ~/.config/google-chrome-beta/Default/Preferences
+sudo -H -u coderdojochi bash -c 'notify-send --urgency=low "Chrome: change startup URL to google.com"'
+sed -i 's/"restore_on_startup_migrated":true,/"restore_on_startup":4,"restore_on_startup_migrated":true,"startup_urls":["https:\/\/google.com\/"],/' /home/coderdojochi/.config/google-chrome-beta/Default/Preferences
 
 # Chrome: turn off custome frame
-notify-send --urgency=low "Chrome: turn off custome frame"
-sudo sed -i 's/"window_placement"/"clear_lso_data_enabled":true,"custom_chrome_frame":false,"pepper_flash_settings_enabled":true,"window_placement"/' ~/.config/google-chrome-beta/Default/Preferences
+sudo -H -u coderdojochi bash -c 'notify-send --urgency=low "Chrome: turn off custome frame"'
+sed -i 's/"window_placement"/"clear_lso_data_enabled":true,"custom_chrome_frame":false,"pepper_flash_settings_enabled":true,"window_placement"/' /home/coderdojochi/.config/google-chrome-beta/Default/Preferences
 
-sudo sed -i 's/"cjpalhdlnbpafiamejdnhcphjbkeiagm":{/"cjpalhdlnbpafiamejdnhcphjbkeiagm":{"browser_action_visible":false,/' ~/.config/google-chrome-beta/Default/Preferences
+sed -i 's/"cjpalhdlnbpafiamejdnhcphjbkeiagm":{/"cjpalhdlnbpafiamejdnhcphjbkeiagm":{"browser_action_visible":false,/' /home/coderdojochi/.config/google-chrome-beta/Default/Preferences
 
 # Setting up the dock
-notify-send --urgency=low "Setting up the dock"
-rm ~/.config/plank/dock1/launchers/*.dockitem
+sudo -H -u coderdojochi bash -c 'notify-send --urgency=low "Setting up the dock"'
+rm /home/coderdojochi/.config/plank/dock1/launchers/*.dockitem
 
-wget -qLO ~/.config/plank/dock1/launchers/pantheon-files.dockitem "https://raw.githubusercontent.com/CoderDojoChi/linux-update/master/pantheon-files.dockitem"
-wget -qLO ~/.config/plank/dock1/launchers/gedit.dockitem "https://raw.githubusercontent.com/CoderDojoChi/linux-update/master/gedit.dockitem"
-wget -qLO ~/.config/plank/dock1/launchers/google-chrome-beta.dockitem "https://raw.githubusercontent.com/CoderDojoChi/linux-update/master/google-chrome-beta.dockitem"
+wget -qLO /home/coderdojochi/.config/plank/dock1/launchers/pantheon-files.dockitem "https://raw.githubusercontent.com/CoderDojoChi/linux-update/master/pantheon-files.dockitem"
+wget -qLO /home/coderdojochi/.config/plank/dock1/launchers/gedit.dockitem "https://raw.githubusercontent.com/CoderDojoChi/linux-update/master/gedit.dockitem"
+wget -qLO /home/coderdojochi/.config/plank/dock1/launchers/google-chrome-beta.dockitem "https://raw.githubusercontent.com/CoderDojoChi/linux-update/master/google-chrome-beta.dockitem"
 
-sed -i 's/HideMode=3/HideMode=0/g' ~/.config/plank/dock1/settings
+sed -i 's/HideMode=3/HideMode=0/g' /home/coderdojochi/.config/plank/dock1/settings
 
 # Changing desktop background
-notify-send --urgency=low "Changing desktop background"
-sudo wget -qLO /usr/share/backgrounds/coderdojochi.png "https://raw.githubusercontent.com/CoderDojoChi/linux-update/master/coderdojochi.png"
+sudo -H -u coderdojochi bash -c 'notify-send --urgency=low "Changing desktop background"'
+wget -qLO /usr/share/backgrounds/coderdojochi.png "https://raw.githubusercontent.com/CoderDojoChi/linux-update/master/coderdojochi.png"
 gsettings set 'org.gnome.desktop.background' 'picture-uri' 'file:///usr/share/backgrounds/coderdojochi.png'
 gsettings set 'org.gnome.desktop.background' 'picture-options' 'zoom'
 
 # Setting screensaver settings
-notify-send --urgency=low "Setting screensaver settings"
+sudo -H -u coderdojochi bash -c 'notify-send --urgency=low "Setting screensaver settings"'
 gsettings set 'org.gnome.desktop.screensaver' 'lock-delay' 3600
 gsettings set 'org.gnome.desktop.screensaver' 'lock-enabled' false
 gsettings set 'org.gnome.desktop.screensaver' 'idle-activation-enabled' false
 
 killall plank
 
-sudo wget -qLO /usr/share/X11/xorg.conf.d/60-drag-and-drop-quirk.conf "https://raw.githubusercontent.com/CoderDojoChi/linux-update/master/60-drag-and-drop-quirk.conf"
+wget -qLO /usr/share/X11/xorg.conf.d/60-drag-and-drop-quirk.conf "https://raw.githubusercontent.com/CoderDojoChi/linux-update/master/60-drag-and-drop-quirk.conf"
 
 # Cleanup
-notify-send --urgency=low "Cleanup"
-sudo apt-get -q=2 autoremove -y
-sudo apt-get -q=2 autoclean -y
-sudo rm -rf {/root,/home/*}/.local/share/zeitgeist
+sudo -H -u coderdojochi bash -c 'notify-send --urgency=low "Cleanup"'
+apt-get -q=2 autoremove -y
+apt-get -q=2 autoclean -y
+rm -rf {/root,/home/*}/.local/share/zeitgeist
 
 # Remove old files that students might of saved
-sudo rm -rf /home/coderdojochi/Downloads/*
-sudo rm -rf /home/coderdojochi/Documents/*
-sudo rm -rf /home/coderdojochi/Music/*
-sudo rm -rf /home/coderdojochi/Pictures/*
-sudo rm -rf /home/coderdojochi/Videos/*
-sudo rm -rf /home/coderdojochi/Public/*
-sudo rm -rf /home/coderdojochi/Templates/*
+rm -rf /home/coderdojochi/Downloads/*
+rm -rf /home/coderdojochi/Documents/*
+rm -rf /home/coderdojochi/Music/*
+rm -rf /home/coderdojochi/Pictures/*
+rm -rf /home/coderdojochi/Videos/*
+rm -rf /home/coderdojochi/Public/*
+rm -rf /home/coderdojochi/Templates/*
 
 # Restarting in 1 minute
-notify-send --urgency=low "Restarting in 1 minute"
-sudo shutdown -r 1
+sudo -H -u coderdojochi bash -c 'notify-send --urgency=low "Restarting in 1 minute"'
+shutdown -r 1

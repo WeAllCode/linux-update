cb761b0e502766099264e44f929d2e408f901804
Ali Karbassi <ali@karbassi.com>
2016-07-19 00:49:37 -0500

    Delete any old sources file.

--- update.sh
+++ update.sh
@@ -61,6 +61,8 @@ userrun 'notify-send --urgency=critical "Update Script Running"'
 # Adding Google to package manager
 output "Adding Google to package manager"
 wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | apt-key add -
+
+rm -rf /etc/apt/sources.list.d/google-chrome.list*
 wget -qLO /etc/apt/sources.list.d/google-chrome.list \
      "$URL/etc/apt/sources.list.d/google-chrome.list"
 

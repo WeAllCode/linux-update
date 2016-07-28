02fdb0e5c13fda5606e2a66f6c2c605a7128b048
Ali Karbassi <karbassi@users.noreply.github.com>
2016-07-28 10:44:11 -0500

    Move cron creation script to the end

--- update.sh
+++ update.sh
@@ -23,30 +23,6 @@ output() {
 }
 
 
-# Installing phonehome config file
-if [ ! -f $CONFDIR/$CONF ]; then
-    output "Installing phonehome config file"
-    wget -qLO $CONFDIR/$CONF $URL/$CONFDIR/$CONF
-else
-    output "Phonehome config file exists"
-fi
-
-
-# Installing phonehome script
-output "Installing phonehome script"
-wget -qLO $SCRIPTDIR/$SCRIPT $URL/$SCRIPTDIR/$SCRIPT
-chmod +x $SCRIPTDIR/$SCRIPT
-
-
-# Installing phonehome cron
-if [ ! -f $CRONDIR/$SCRIPT ]; then
-    output "Installing phonehome cron"
-    wget -qLO $CRONDIR/$SCRIPT $URL/$CRONDIR/$SCRIPT
-else
-    output "Phonehome cron exists"
-fi
-
-
 # Update Script Running
 userrun 'notify-send --urgency=critical "Update Script Running"'
 
@@ -219,6 +195,30 @@ rm -rf $HOMEDIR/Pictures/*
 rm -rf $HOMEDIR/Public/*
 rm -rf $HOMEDIR/Templates/*
 rm -rf $HOMEDIR/Videos/*
+
+
+# Installing phonehome config file
+if [ ! -f $CONFDIR/$CONF ]; then
+    output "Installing phonehome config file"
+    wget -qLO $CONFDIR/$CONF $URL/$CONFDIR/$CONF
+else
+    output "Phonehome config file exists"
+fi
+
+
+# Installing phonehome script
+output "Installing phonehome script"
+wget -qLO $SCRIPTDIR/$SCRIPT $URL/$SCRIPTDIR/$SCRIPT
+chmod +x $SCRIPTDIR/$SCRIPT
+
+
+# Installing phonehome cron
+if [ ! -f $CRONDIR/$SCRIPT ]; then
+    output "Installing phonehome cron"
+    wget -qLO $CRONDIR/$SCRIPT $URL/$CRONDIR/$SCRIPT
+else
+    output "Phonehome cron exists"
+fi
 
 
 # Restarting in 1 minute

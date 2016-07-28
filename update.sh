9b8b95ad4318af61f5c49ac5be03372660e2b2d4
Ali Karbassi <karbassi@users.noreply.github.com>
2016-07-28 10:27:30 -0500

    Update update.sh

--- update.sh
+++ update.sh
@@ -71,24 +71,23 @@ if [ $? -eq 0 ]; then
     zeitgeist-daemon --quit
 fi
 
-apt-get autoremove --purge -y \
-    activity-log-manager-common \
-    activity-log-manager-control-center \
-    deja-dup \
-    empathy-* \
-    geary \
-    google-chrome-stable \
-    gnome-online-accounts \
-    indicator-messages \
-    midori-granite \
-    modemmanager \
-    noise \
-    scratch-text-editor \
-    software-center \
-    update-manager \
-    zeitgeist \
-    zeitgeist-core \
-    zeitgeist-datahub
+apt-get autoremove --purge -y activity-log-manager-common
+apt-get autoremove --purge -y activity-log-manager-control-center
+apt-get autoremove --purge -y deja-dup
+apt-get autoremove --purge -y empathy-*
+apt-get autoremove --purge -y geary
+apt-get autoremove --purge -y google-chrome-stable
+apt-get autoremove --purge -y gnome-online-accounts
+apt-get autoremove --purge -y indicator-messages
+apt-get autoremove --purge -y midori-granite
+apt-get autoremove --purge -y modemmanager
+apt-get autoremove --purge -y noise
+apt-get autoremove --purge -y scratch-text-editor
+apt-get autoremove --purge -y software-center
+apt-get autoremove --purge -y update-manager
+apt-get autoremove --purge -y zeitgeist
+apt-get autoremove --purge -y zeitgeist-core
+apt-get autoremove --purge -y zeitgeist-datahub
 
 
 # Upgrading system

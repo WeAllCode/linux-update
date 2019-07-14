9d28fe98726469c1759ced161547323f7fbf177d
Ali Karbassi <ali@karbassi.com>
2019-07-13 21:01:32 -0500

    disable notify-send

--- update.sh
+++ update.sh
@@ -4,7 +4,7 @@
 # This script updates all We All Code computers.
 #
 
-VERSION="2.0.1"
+VERSION="2.0.2"
 
 URL="https://raw.githubusercontent.com/WeAllCode/linux-update/juno"
 
@@ -31,7 +31,7 @@ userrun() {
 
 output() {
     echo "\n\n####################\n# $1\n####################\n\n";
-    userrun "notify-send --urgency=low '$1'";
+    # userrun "notify-send --urgency=low '$1'";
 }
 
 debInst() {
